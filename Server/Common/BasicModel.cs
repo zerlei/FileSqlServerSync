73950a1384a49365b0fea59205a5123331698a66
zerlei <1445089819@qq.com>
2024-07-01 23:43:00 +0800

    基础类修改了一些

--- Server/Common/BasicModel.cs
+++ Server/Common/BasicModel.cs
@@ -10,23 +10,33 @@ public enum DirOrFile
 
 public abstract class AFileOrDir
 {
+    public static string RootDir { get; set; } = "/";
     public DirOrFile Type { get; set; }
     public required string RelativePath { get; set; }
 }
 
-public class File :AFileOrDir
+public class File : AFileOrDir
 {
     public DateTime MTime { get; set; }
 }
 
-public class Dir :AFileOrDir
+public class Dir : AFileOrDir
 {
     public List<AFileOrDir> Children { get; set; }
+
     [SetsRequiredMembers]
     public Dir(string relativePath)
     {
         RelativePath = relativePath;
-        Children = [new File { RelativePath = "1.txt", MTime = DateTime.Now,Type = DirOrFile.File }];
+        Children =
+        [
+            new File
+            {
+                RelativePath = "1.txt",
+                MTime = DateTime.Now,
+                Type = DirOrFile.File
+            }
+        ];
         var x = new Dir("");
     }
 }
