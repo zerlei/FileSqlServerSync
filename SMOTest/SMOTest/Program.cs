f8f1cf6a50971b57ba7f20e5b4539620b8292783
zerlei <1445089819@qq.com>
2024-09-09 08:38:04 +0800

    chore: add SMOtest

---
+++ SMOTest/SMOTest/Program.cs
@@ -0,0 +1,17 @@
+﻿//using System;
+//using Microsoft.SqlServer.Management.Common;
+//using Microsoft.SqlServer.Management.Smo;
+
+//public class A
+//{
+//    public static void Main()
+//    {
+//        // For remote connection, remote server name / ServerInstance needs to be specified
+//        ServerConnection srvConn2 = new ServerConnection("localhost");
+//        srvConn2.LoginSecure = false;
+//        srvConn2.Login = "sa";
+//        srvConn2.Password = "0";
+//        Server srv3 = new Server(srvConn2);
+//        Console.WriteLine(srv3.Information.Version); // connection is established
+//    }
+//}
---
+++ SMOTest/SMOTest/client.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+
+namespace DatabaseStructureOnlyExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string serverName = "your_server_name";
+            string sourceDatabaseName = "source_database_name";
+            string targetDatabaseName = "target_database_name";
+
+            // 连接到 SQL Server 实例
+            ServerConnection serverConnection = new ServerConnection(serverName);
+            Server server = new Server(serverConnection);
+
+            // 创建目标数据库
+            Database targetDatabase = new Database(server, targetDatabaseName);
+            targetDatabase.Create();
+
+            // 获取源数据库
+            Database sourceDatabase = server.Databases[sourceDatabaseName];
+
+            // 创建一个脚本生成器
+            Scripter scripter = new Scripter(server)
+            {
+                Options =
+                {
+                    ScriptDrops = false, // 不生成删除脚本
+                    WithDependencies = true, // 包括所有相关对象
+                    IncludeIfNotExists = false, // 不包含如果不存在的语句
+                    IncludeSchema = true, // 包含架构
+                    IncludeData = false // 不包含数据
+                }
+            };
+
+            // 生成表的创建脚本
+            foreach (Table table in sourceDatabase.Tables)
+            {
+                if (!table.IsSystemObject)
+                {
+                    var script = scripter.Script(new Urn[] { table.Urn });
+                    foreach (var line in script)
+                    {
+                        // 将脚本执行到目标数据库
+                        server.ConnectionContext.ExecuteNonQuery(line);
+                    }
+                }
+            }
+
+            Console.WriteLine($"结构仅副本 '{targetDatabaseName}' 创建成功。");
+        }
+    }
+}
