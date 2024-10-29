2037f1ac4a0a049275ac25a4a66372bab5abc6aa
ZhaoLei <zhaolei@hacint.com.cn>
2024-10-29 13:13:22 +0800

    feat: 脚本或工具对应修改增加命令行扩展

--- Tool/webtool/src/App.vue
+++ Tool/webtool/src/App.vue
@@ -16,22 +16,33 @@ let Pipe = null
 const Msgs = ref([])
 const code = ref(`
 config = {
-  Name: "Test",
-  RemoteUrl: "127.0.0.1:6819",
-  RemotePwd: "t123",
-  IsDeployDb: false,
+  //发布的名称，每个项目具有唯一的一个名称
+  Name: "FYMF",
+  RemotePwd: "FYMF",
+  //远程服务器地址，也就是发布的目的地，它是正式环境
+  RemoteUrl: "127.0.0.1:8007",
+  //是否发布数据库 sqlserver
+  IsDeployDb: true,
+  //是否发布前重新构建项目
   IsDeployProject: false,
+  //项目地址
   LocalProjectAbsolutePath:
     "D:/git/HMES-H7-HNFY/HMES-H7-HNFYMF/HMES-H7-HNFYMF.WEB",
+  //源文件目录地址，是要发布的文件根目录，它是绝对路径，!执行发布时将发布到这个目录!
   LocalRootPath: "D:/FileSyncTest/src",
-
-  RemoteRootPath: "D:/FileSyncTest/dst",
+  //目标文件目录地址，也就是部署服务的机器上的项目文件根目录，它是绝对路径
+  RemoteRootPath: "D:/FYMF",
+  //源数据库配置 SqlServer,将会同步数据库的结构
   SrcDb: {
+    //Host
     ServerName: "172.16.12.2",
+    //数据库名
     DatabaseName: "HMES_H7_HNFYMF",
     User: "hmes-h7",
     Password: "Hmes-h7666",
+    //是否信任服务器证书
     TrustServerCertificate: "True",
+    //同步的数据，这些数据将会同步
     SyncTablesData: [
       "dbo.sys_Button",
       "dbo.sys_Menu",
@@ -39,19 +50,39 @@ config = {
       "dbo.sys_Page",
     ],
   },
+  //目标数据库配置 sqlserver
   DstDb: {
     ServerName: "127.0.0.1",
     DatabaseName: "HMES_H7_HNFYMF",
     User: "sa",
-    Password: "0",
+    Password: "Yuanmo520...",
     TrustServerCertificate: "True",
   },
+  //子目录配置，每个子目录都有自己不同的发布策略，它是相对路径，即相对于LocalRootPath和RemoteRootPath(注意 '/'，这将拼成一个完整的路径)，文件数据依此进行,
   DirFileConfigs: [
     {
       DirPath: "/bin",
+      //排除的文件或目录，它是相对路径，相对于！！！LocalRootPath和RemoteRootPath！！！
       Excludes: ["/roslyn", "/Views"],
+      //只追踪文件或目录，它是相对路径，相对于！！！LocalRootPath和RemoteRootPath！！！，它的优先级最高，如果你指定了它的值，Excludes将会失效
+      // CherryPicks:[]
     },
   ],
+  ExecProcesses: [],
+  //  ExecProcesses:[
+  //   {
+  //     // 参数
+  //     Argumnets:"ls",
+  //     //  执行命令位置
+  //     FileName:"powershell",
+  //     // 相关步骤开始之前（B）或之后 (A)
+  //     StepBeforeOrAfter:"A",
+  //     // 本地（L）或远程 (R) 执行
+  //     ExecInLocalOrServer:"L",
+  //     // 步骤 1. 连接远程 2. 发布项目 3. 文件对比 4. 提取sqlserver 5. 打包上传 6. 发布
+  //     Step:1
+  //   }
+  //  ]
 };
 `)
 var CStatus = ref('None')
